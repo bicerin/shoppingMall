7ecc5a930d7ce7101492a0b2e53ffca5c9b4a5da
USER <USER@USER8-PC>
2017-10-18 19:08:27 +0900

    곱셈 추가

--- GitTest/src/Calculator.java
+++ GitTest/src/Calculator.java
@@ -3,9 +3,13 @@ public class Calculator {
 	public int sum(int num1, int num2) {
 		return num1 + num2;
 	}
+	
 	public int substraction(int num1, int num2) {
 		return num1 - num2;
 	}
 	
+	public int multiplication(int num1, int num2) {
+		return num1 * num2;
+	}
 
 }
